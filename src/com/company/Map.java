395fd4055d33bc43f1868c30049b878951f3df09
unknown <amashanov@inbox.ru>
2019-11-15 19:04:30 +0300

    first commit

---
+++ src/com/company/Map.java
@@ -0,0 +1,104 @@
+package com.company;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Map {
+      private class Path{
+        private String [] points;
+        private int time;
+        Path(String points[],int time){
+            this.points=points;
+            this.time=time;
+        }
+        public String showPath(){
+            return (points[0]+" "+points[1]+" "+time);
+        }
+    }
+    private ArrayList<Path> paths;
+    private HashMap<String,Integer> points;
+    private ArrayList pot;
+    private int ways[][];
+     Map(){
+         paths=new ArrayList<>();
+         points=new HashMap<String,Integer>();
+         pot=new ArrayList();
+         ways=new int[100][100];
+         for (int i=0;i<100;i++)
+             for(int j=0;j<100;j++)
+                 ways[i][j]=1000000;
+     }
+    public void addPath(String place1,String place2,int time){
+        String[] arr=new String[2];
+        arr[0]=place1; arr[1]=place2;
+        Path path=new Path(arr,time);
+        paths.add(path);
+        if (!pot.contains(place1))
+            pot.add(place1);
+        if (!pot.contains(place2))
+            pot.add(place2);
+        if (!points.containsKey(place1))
+            points.put(place1,points.size());
+        if (!points.containsKey(place2))
+            points.put(place2,points.size());
+         //делаем матрицу смежности
+        ways[pot.indexOf(place1)][pot.indexOf(place2)]=time;
+        ways[pot.indexOf(place2)][pot.indexOf(place1)]=time;
+
+    }
+    public ArrayList getAllPlace(){
+        return paths;
+    }
+
+    public ArrayList genShortWay(String place1, String place2){
+
+         int MAX=1000000; int pointCount=pot.size();
+        int D[]=new int[pointCount]; int P[]=new int[pointCount]; int startPoint=pot.indexOf(place1);//int startPoint=points.get(place1);
+        boolean []visited=new boolean[pointCount];
+        Arrays.fill(D,1000000); Arrays.fill(P,-1);
+        Arrays.fill(visited,false);
+        D[startPoint]=0;
+        for (int i=0;i<pointCount;i++)
+            D[i]=ways[startPoint][i];
+        int index=0,u=0;
+        for (int i=0;i<pointCount;i++){
+            int min=MAX;
+            for (int j=0;j<pointCount;j++){
+                if (!visited[j] &&D[j]<min){
+                    min=D[j]; index=j;
+                }
+            }
+            u=index;
+            visited[u]=true;
+            for (int j=0;j<pointCount;j++){
+                if (!visited[j] && ways[u][j]!=MAX && D[u]!=MAX && (D[u]+ways[u][j]<D[j])){
+                    D[j]=D[u]+ways[u][j];
+                    P[j]=u;
+                }
+            }
+        }
+
+        ArrayList stack=new ArrayList();
+        int target=pot.indexOf(place2);
+        for (int v=target;v!=-1;v=P[v])
+            stack.add(0,v);
+        ArrayList sp=new ArrayList(stack.size());
+        for (int i=0;i<stack.size();i++)
+            sp.add(stack.get(i));
+        ArrayList res= new ArrayList();
+        res.add(place1);
+        for (int i=0; i<sp.size();i++)
+            res.add(pot.get((Integer)sp.get(i)));
+        System.out.println(res.toString());
+        return res;
+    }
+    public String show(){
+         String res="";
+         for (int i=0; i<paths.size();i++){
+             res+=paths.get(i).showPath()+"\n";
+         }
+         return res;
+    }
+}
---
+++ src/com/company/PizzaSystem.java
@@ -0,0 +1,85 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PizzaSystem {
+    public static void main(String[] args) {
+        ArrayList<User> users=new ArrayList<>();
+        Map map=new Map();
+        User currentUser=new User();
+        String currentLocation="";
+        Client a=new Client("Alex","A","1234","Москва");
+        Client b=new Client("Ivan","I","1234","Подольск");
+        users.add(a);users.add(b);
+        System.out.println(findUser(users,"A","1234").getPlace());
+        map.addPath("Подольск","Москва",10);map.addPath("Подольск","Климовск",2);map.addPath("Климовск","Москва",7);
+        map.addPath("Москва","A",2); map.addPath("B","A",2);
+        Scanner in=new Scanner(System.in);
+        System.out.println("1.Добавить пользователя"+'\n'+"2.Добавить путь"+'\n'+"3.Войти в систему"+'\n'+"4.Ввести место курьера"+'\n'+"5.Оформить заказ(получить путь и время)");
+        int n=0;
+        String str;
+        while (n!=8){
+            System.out.println("Выберите действие");
+            n=in.nextInt();
+            switch(n){
+                case 1:
+                    System.out.println("Введите тип (1-админ и ничего не делает)"); int t=in.nextInt(); in.nextLine();
+                    System.out.println("Введите имя"); String s1=in.nextLine();
+                    System.out.println("Введите логин"); String s2=in.nextLine();
+                    System.out.println("Введите пароль"); String s3=in.nextLine();
+                    if (t!=1)
+                    {System.out.println("Введите место"); String s4=in.nextLine(); users=addClient(users,s1,s2,s3,s4);}
+                    else
+                        users=addUser(users,s1,s2,s3,s3);
+                    break;
+                case 2:
+                    System.out.println("Введите первое место"); in.nextLine(); String place1=in.nextLine();
+                    System.out.println("Введите второе место"); String place2=in.nextLine();
+                    System.out.println("Введите время");int time=in.nextInt();
+                    map.addPath(place1,place2,time);
+                    break;
+                case 3:
+                    System.out.println("Введите логин"); in.nextLine();String login=in.nextLine();
+                    System.out.println("Введите пароль"); String password=in.nextLine();
+                    if (findUser(users,login,password)!=null){
+                        currentUser=findUser(users,login,password);
+                        System.out.println("Вы вошли в систему как "+currentUser.getLogin());
+                        if (currentUser.getPlace()!=null)
+                            System.out.println("Ваше местоположение: "+currentUser.getPlace());
+                    }
+                    else System.out.println("Неверные данные");
+                    break;
+                case 4:
+                    System.out.println("Введите текущую локацию");in.nextLine(); String loc=in.nextLine();
+                    currentLocation=loc; break;
+                case 5:
+                    if (currentUser==null || currentLocation=="" || currentUser.getPlace()==null){
+                        System.out.println("Необходимо войти в систему и ввести текущее место курьера"); break;
+                    }
+                    map.genShortWay(currentLocation,currentUser.getPlace());
+            }
+
+        }
+    }
+
+    public static ArrayList<User> addUser(ArrayList<User>users,String name, String login, String password, String rep){
+        users.add(new User(name,login,password));
+        return(users);
+    }
+    public static ArrayList<User> addClient(ArrayList<User>users, String name, String login, String password, String place){
+        users.add(new Client(name,login,password,place));
+        return(users);
+    }
+    public static User findUser(ArrayList<User>users,String login, String password){
+        for (User i: users
+             ) {
+            String s1=i.getLogin(); String s2=i.getPassword();
+        if ((s1.equals(login)) && (s2.equals(password)))
+            return i;
+        }
+        return null;
+    }
+
+}
